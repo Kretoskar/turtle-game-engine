716e8768a5337d67128585f3c56e56ff7cbd4ea0
Oskar Kogut <kogutoskar123@gmail.com>
2024-09-09 15:19:47 +0200

    Make console window docked with fixed size

--- src/Turtle/Rendering/UserInterface.cpp
+++ src/Turtle/Rendering/UserInterface.cpp
@@ -8,6 +8,7 @@ bool Turtle::UserInterface::Init(GLFWwindow* window)
 {
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
+    ImGui::StyleColorsDark();
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     const char* glslVersion = "#version 440 core";
@@ -22,11 +23,20 @@ void Turtle::UserInterface::CreateFrame()
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
+    ImVec2 HorCenterVerBottomPos = ImGui::GetMainViewport()->GetCenter();
+    HorCenterVerBottomPos.y = ImGui::GetMainViewport()->Size.y;
+
+    ImVec2 ConsoleWindowSize = ImGui::GetMainViewport()->Size;
+    ConsoleWindowSize.y /= 4;
+
     if (Turtle::Logger::loggedLineBufferCurrCount > 0)
     {
         ImGuiWindowFlags flags = 0;
         ImGui::SetNextWindowBgAlpha(0.8f);
+        ImGui::SetNextWindowPos(HorCenterVerBottomPos, ImGuiCond_Always, ImVec2(0.5f, 1.0f));
+        ImGui::SetNextWindowSize(ConsoleWindowSize, ImGuiCond_Always);
         ImGui::Begin("Console", nullptr, flags);
+
 
         for (unsigned int i = 0; i < Turtle::Logger::loggedLineBufferCurrCount; i++)
         {
